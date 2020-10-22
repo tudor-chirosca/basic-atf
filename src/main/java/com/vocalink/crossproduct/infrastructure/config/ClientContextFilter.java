4492f88043f16ebe20bf2ae72576e8696ab2525f
Gleb Godonoga <gleb.godonoga@gmail.com>
2020-10-22 15:04:54 +0300

    refactor: Enable swagger ui and update filter whitelist

--- src/main/java/com/vocalink/crossproduct/infrastructure/config/ClientContextFilter.java
+++ src/main/java/com/vocalink/crossproduct/infrastructure/config/ClientContextFilter.java
@@ -1,55 +1,49 @@
 package com.vocalink.crossproduct.infrastructure.config;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Set;
-import javax.servlet.Filter;
+
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j
 @Component("applicationContextFilter")
-public class ClientContextFilter implements Filter {
+public class ClientContextFilter extends OncePerRequestFilter {
 
   @Value("${app.context}")
   private Set<String> contexts;
 
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
-    //no implementation
-  }
+  @Value("${app.infra-endpoints}")
+  private String[] whitelistedEndpoints;
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-    HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-    HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+  protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain chain)
+    throws ServletException, IOException {
     log.info("Applying filter for : {}",
         httpServletRequest.getRequestURI());
 
     String contextHeader = httpServletRequest.getHeader("context");
-
-    if (!contexts.contains(contextHeader) &&
-        !(httpServletRequest.getServletPath().contains("/actuator/health"))) {
-      httpServletResponse.sendError(HttpStatus.BAD_REQUEST.value(), "Invalid context");
-    } else {
-      chain.doFilter(request, response);
-      log.info("Passing filter validation : {}",
-          httpServletRequest.getRequestURI());
+    if (contexts.contains(contextHeader)) {
+      chain.doFilter(httpServletRequest, httpServletResponse);
+      return;
     }
+    httpServletResponse.sendError(HttpStatus.BAD_REQUEST.value(), "Invalid context");
   }
 
   @Override
-  public void destroy() {
-    //no implementation
+  protected boolean shouldNotFilter(HttpServletRequest request) {
+    return Arrays.stream(whitelistedEndpoints)
+      .anyMatch(e -> new AntPathMatcher().match(e, request.getServletPath()));
   }
-
 }
--- src/main/java/com/vocalink/crossproduct/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/vocalink/crossproduct/infrastructure/config/SwaggerConfig.java
@@ -3,17 +3,13 @@ package com.vocalink.crossproduct.infrastructure.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
-@ConditionalOnProperty(value = "swagger.enabled", havingValue = "true", matchIfMissing = false)
+@ConditionalOnProperty(value = "swagger.enabled", havingValue = "true")
 public class SwaggerConfig {
 
   @Bean
@@ -25,19 +21,5 @@ public class SwaggerConfig {
         .select()
         .apis(RequestHandlerSelectors.basePackage("com.vocalink.crossproduct.ui"))
         .build();
-  }
-
-  @Bean
-  public WebMvcConfigurer swaggerUi() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-            .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-            .addResourceLocations("classpath:/META-INF/resources/webjars/");
-      }
-    };
   }
 }
