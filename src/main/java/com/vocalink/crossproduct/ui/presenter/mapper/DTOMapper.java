7aef11c6d807b2034efc794edcb892f114cc22b7
Dmitrii Nedealcov <dmitrii.nedealcov@endava.com>
2020-10-23 14:24:05 +0300

    refactor: removed model mapper. refactored presenter mapper

--- src/main/java/com/vocalink/crossproduct/ui/presenter/mapper/DTOMapper.java
+++ src/main/java/com/vocalink/crossproduct/ui/presenter/mapper/DTOMapper.java
@@ -3,13 +3,19 @@ package com.vocalink.crossproduct.ui.presenter.mapper;
 import com.vocalink.crossproduct.domain.cycle.Cycle;
 import com.vocalink.crossproduct.domain.io.IODetails;
 import com.vocalink.crossproduct.domain.participant.Participant;
+import com.vocalink.crossproduct.domain.position.IntraDayPositionGross;
+import com.vocalink.crossproduct.domain.position.PositionDetails;
 import com.vocalink.crossproduct.ui.dto.cycle.CycleDto;
 import com.vocalink.crossproduct.ui.dto.io.IODetailsDto;
+import com.vocalink.crossproduct.ui.dto.participant.ParticipantDto;
+import com.vocalink.crossproduct.ui.dto.position.IntraDayPositionGrossDto;
 import com.vocalink.crossproduct.ui.dto.position.PositionDetailsDto;
 import com.vocalink.crossproduct.ui.dto.position.PositionDetailsTotalsDto;
+import java.math.BigInteger;
 import java.time.LocalDate;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -22,15 +28,32 @@ public interface DTOMapper {
   @Mapping(target = "dateFrom", source = "date")
   IODetailsDto toDto(IODetails ioDetails, Participant participant, LocalDate date);
 
-  default PositionDetailsTotalsDto toDto(PositionDetailsDto details) {
+  @Mapping(target = "totalCredit", source = "details", qualifiedByName = "countCredit")
+  @Mapping(target = "totalDebit", source = "details", qualifiedByName = "countDebit")
+  @Mapping(target = "totalNetPosition", source = "details", qualifiedByName = "countNetPosition")
+  PositionDetailsTotalsDto toDto(PositionDetailsDto details);
 
-    return PositionDetailsTotalsDto.builder()
-        .totalCredit(details.getCustomerCreditTransfer().getCredit().add(
-            details.getPaymentReturn().getCredit()))
-        .totalDebit(details.getCustomerCreditTransfer().getDebit().add(
-            details.getPaymentReturn().getDebit()))
-        .totalNetPosition(details.getCustomerCreditTransfer().getNetPosition().add(
-            details.getPaymentReturn().getNetPosition()))
-        .build();
+  ParticipantDto toDto(Participant participant);
+
+  PositionDetailsDto toDto(PositionDetails positionDetails);
+
+  IntraDayPositionGrossDto toDto(IntraDayPositionGross intraDayPositionGross);
+
+  @Named("countCredit")
+  static BigInteger countCredit(PositionDetailsDto details) {
+    return details.getCustomerCreditTransfer().getCredit()
+        .add(details.getPaymentReturn().getCredit());
+  }
+
+  @Named("countDebit")
+  static BigInteger countDebit(PositionDetailsDto details) {
+    return details.getCustomerCreditTransfer().getDebit()
+        .add(details.getPaymentReturn().getDebit());
+  }
+
+  @Named("countNetPosition")
+  static BigInteger countNetPosition(PositionDetailsDto details) {
+    return details.getCustomerCreditTransfer().getNetPosition()
+        .add(details.getPaymentReturn().getNetPosition());
   }
 }
--- src/main/java/com/vocalink/crossproduct/ui/presenter/mapper/SelfFundingSettlementDetailsMapper.java
+++ src/main/java/com/vocalink/crossproduct/ui/presenter/mapper/SelfFundingSettlementDetailsMapper.java
@@ -9,19 +9,13 @@ import com.vocalink.crossproduct.domain.position.PositionDetails;
 import com.vocalink.crossproduct.ui.dto.ParticipantSettlementDetailsDto;
 import com.vocalink.crossproduct.ui.dto.cycle.CycleDto;
 import com.vocalink.crossproduct.ui.dto.participant.ParticipantDto;
-import com.vocalink.crossproduct.ui.dto.position.IntraDayPositionGrossDto;
 import com.vocalink.crossproduct.ui.dto.position.PositionDetailsDto;
 import com.vocalink.crossproduct.ui.dto.position.PositionDetailsTotalsDto;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class SelfFundingSettlementDetailsMapper {
-
-  final private ModelMapper modelMapper = new ModelMapper();
 
   public ParticipantSettlementDetailsDto presentFullParticipantSettlementDetails(
       List<Cycle> cycles,
@@ -30,44 +24,39 @@ public class SelfFundingSettlementDetailsMapper {
       Participant fundingParticipant,
       IntraDayPositionGross intradayPositionGross) {
 
-    ParticipantDto participantDto = modelMapper.map(participant, ParticipantDto.class);
+    ParticipantDto participantDto = MAPPER.toDto(participant);
+
     CycleDto currentCycleDto = MAPPER.toDto(cycles.get(1));
     CycleDto previousCycleDto = MAPPER.toDto(cycles.get(0));
 
-    PositionDetailsDto currentPositionDetailsDto = modelMapper
-        .map(positionsDetails.get(1), PositionDetailsDto.class);
-    PositionDetailsDto previousPositionDetailsDto = modelMapper
-        .map(positionsDetails.get(0), PositionDetailsDto.class);
+    PositionDetailsDto currentPositionDetailsDto = MAPPER.toDto(positionsDetails.get(1));
+    PositionDetailsDto previousPositionDetailsDto = MAPPER.toDto(positionsDetails.get(0));
 
     PositionDetailsTotalsDto currentPositionDetailsTotalsDto = MAPPER.toDto(currentPositionDetailsDto);
     PositionDetailsTotalsDto previousPositionDetailsTotalsDto = MAPPER.toDto(previousPositionDetailsDto);
 
-    if (fundingParticipant != null && intradayPositionGross != null) {
-      ParticipantDto fundingParticipantDto = modelMapper.map(fundingParticipant, ParticipantDto.class);
-      IntraDayPositionGrossDto intradayPositionGrossDto = modelMapper
-          .map(intradayPositionGross, IntraDayPositionGrossDto.class);
-
+    if (fundingParticipant == null || intradayPositionGross == null) {
       return ParticipantSettlementDetailsDto.builder()
           .participant(participantDto)
-          .settlementBank(fundingParticipantDto)
           .currentCycle(currentCycleDto)
           .previousCycle(previousCycleDto)
           .currentPosition(currentPositionDetailsDto)
           .previousPosition(previousPositionDetailsDto)
           .currentPositionTotals(currentPositionDetailsTotalsDto)
           .previousPositionTotals(previousPositionDetailsTotalsDto)
-          .intraDayPositionGross(intradayPositionGrossDto)
           .build();
     }
 
     return ParticipantSettlementDetailsDto.builder()
         .participant(participantDto)
+        .settlementBank(MAPPER.toDto(fundingParticipant))
         .currentCycle(currentCycleDto)
         .previousCycle(previousCycleDto)
         .currentPosition(currentPositionDetailsDto)
         .previousPosition(previousPositionDetailsDto)
         .currentPositionTotals(currentPositionDetailsTotalsDto)
         .previousPositionTotals(previousPositionDetailsTotalsDto)
+        .intraDayPositionGross(MAPPER.toDto(intradayPositionGross))
         .build();
   }
 
@@ -75,35 +64,30 @@ public class SelfFundingSettlementDetailsMapper {
       List<Cycle> cycles, List<PositionDetails> positionsDetails, Participant participant,
       Participant fundingParticipant, IntraDayPositionGross intradayPositionGross) {
 
-    ParticipantDto participantDto = modelMapper.map(participant, ParticipantDto.class);
+    ParticipantDto participantDto = MAPPER.toDto(participant);
 
     CycleDto previousCycleDto = MAPPER.toDto(cycles.get(0));
 
-    PositionDetailsDto previousPositionDetailsDto = modelMapper
-        .map(positionsDetails.get(0), PositionDetailsDto.class);
+    PositionDetailsDto previousPositionDetailsDto = MAPPER.toDto(positionsDetails.get(0));
 
     PositionDetailsTotalsDto previousPositionDetailsTotalsDto = MAPPER.toDto(previousPositionDetailsDto);
 
-    if (fundingParticipant != null && intradayPositionGross != null) {
-      ParticipantDto fundingParticipantDto = modelMapper.map(fundingParticipant, ParticipantDto.class);
-      IntraDayPositionGrossDto intradayPositionGrossDto = modelMapper
-          .map(intradayPositionGross, IntraDayPositionGrossDto.class);
-
+    if (fundingParticipant == null || intradayPositionGross == null) {
       return ParticipantSettlementDetailsDto.builder()
           .participant(participantDto)
-          .settlementBank(fundingParticipantDto)
           .previousCycle(previousCycleDto)
           .previousPosition(previousPositionDetailsDto)
           .previousPositionTotals(previousPositionDetailsTotalsDto)
-          .intraDayPositionGross(intradayPositionGrossDto)
           .build();
     }
 
     return ParticipantSettlementDetailsDto.builder()
         .participant(participantDto)
+        .settlementBank(MAPPER.toDto(fundingParticipant))
         .previousCycle(previousCycleDto)
         .previousPosition(previousPositionDetailsDto)
         .previousPositionTotals(previousPositionDetailsTotalsDto)
+        .intraDayPositionGross(MAPPER.toDto(intradayPositionGross))
         .build();
   }
 }
