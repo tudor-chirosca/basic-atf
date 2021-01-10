f3908200fd56b41f3379738577713b0c273b5442
Lluis Santamaria <lluis.santamariasola@mastercard.com>
2021-01-10 15:11:05 +0000

    chore: set up repository tests

---
+++ src/test/kotlin/com/vocalink/crossproduct/infrastructure/bps/config/BPSTestConfig.kt
@@ -0,0 +1,59 @@
+package com.vocalink.crossproduct.infrastructure.bps.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.vocalink.crossproduct.adapter.bps.BPSProperties
+import com.vocalink.crossproduct.adapter.bps.BPSProperties.Detail
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import java.time.Duration
+
+@TestConfiguration
+open class BPSTestConfig {
+
+    @Bean
+    open fun testBpsProperties(): BPSProperties {
+        val props = BPSProperties()
+        props.retryCount = 3
+        props.timeoutDuration = Duration.ofMillis(1000)
+
+        props.baseUrls = hashMapOf(Pair("MOCK", "http://localhost:54001"))
+
+        props.paths = hashMapOf<String, Detail>(
+                Pair("participants-path", Detail("MOCK", "/participants/P27-SEK/readAll")),
+                Pair("participant-path", Detail("MOCK", "/participants/P27-SEK/read")),
+                Pair("cycles-path", Detail("MOCK", "/cycles/readAll")),
+                Pair("position-details-path", Detail("MOCK", "/positionDetails")),
+                Pair("io-participants-path", Detail("MOCK", "/io")),
+                Pair("intra-day-position-gross-path", Detail("MOCK", "/intraDayPosition")),
+                Pair("settlement-position-path", Detail("MOCK", "/settlementPositions/readAll")),
+                Pair("io-details-path", Detail("MOCK", "/io-details")),
+                Pair("alerts-path", Detail("MOCK", "/alerts")),
+                Pair("alert-stats-path", Detail("MOCK", "/alerts/stats")),
+                Pair("alert-threshold-path", Detail("MOCK", "/reference/alerts")),
+                Pair("message-direction-references-path", Detail("MOCK", "/reference/messages")),
+                Pair("alert-stats-path", Detail("MOCK", "/alerts/stats")),
+                Pair("file-references-path", Detail("MOCK", "/reference/files")),
+                Pair("file-enquiries-path", Detail("MOCK", "/enquiry/files")),
+                Pair("batch-enquiries-path", Detail("MOCK", "/enquiry/batches")),
+                Pair("single-file-path", Detail("MOCK", "/enquiry/files/read")),
+                Pair("single-batch-path", Detail("MOCK", "/enquiry/batches/read")),
+                Pair("single-settlement-path", Detail("MOCK", "/enquiry/settlements/read")),
+                Pair("instruction-enquiries-path", Detail("MOCK", "/enquiry/instructions")),
+                Pair("single-settlement-path", Detail("MOCK", "/enquiry/settlements/read")),
+                Pair("settlement-enquiries-path", Detail("MOCK", "/enquiry/settlements"))
+        )
+        props.retryable = listOf(404)
+
+        return props
+    }
+
+    @Bean
+    open fun mapper(): ObjectMapper {
+        val objectMapper = ObjectMapper()
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        objectMapper.registerModule(JavaTimeModule())
+        return objectMapper
+    }
+}
--- src/test/kotlin/com/vocalink/crossproduct/infrastructure/factory/ClientFactoryTest.kt
+++ src/test/kotlin/com/vocalink/crossproduct/infrastructure/factory/ClientFactoryTest.kt
@@ -3,21 +3,15 @@ package com.vocalink.crossproduct.infrastructure.factory
 import com.vocalink.crossproduct.TestConstants
 import com.vocalink.crossproduct.adapter.bps.alert.BPSAlertsClient
 import com.vocalink.crossproduct.adapter.bps.batch.BPSBatchesClient
-import com.vocalink.crossproduct.adapter.bps.cycle.BPSCyclesClient
 import com.vocalink.crossproduct.adapter.bps.files.BPSFilesClient
 import com.vocalink.crossproduct.adapter.bps.io.BPSParticipantIODataClient
-import com.vocalink.crossproduct.adapter.bps.participant.BPSParticipantClient
-import com.vocalink.crossproduct.adapter.bps.positions.BPSPositionClient
 import com.vocalink.crossproduct.adapter.bps.reference.BPSReferencesClient
 import com.vocalink.crossproduct.adapter.bps.settlement.BPSSettlementsClient
 import com.vocalink.crossproduct.infrastructure.exception.ClientNotAvailableException
 import com.vocalink.crossproduct.shared.alert.AlertsClient
 import com.vocalink.crossproduct.shared.batch.BatchesClient
-import com.vocalink.crossproduct.shared.cycle.CyclesClient
 import com.vocalink.crossproduct.shared.files.FilesClient
 import com.vocalink.crossproduct.shared.io.ParticipantIODataClient
-import com.vocalink.crossproduct.shared.participant.ParticipantClient
-import com.vocalink.crossproduct.shared.positions.PositionClient
 import com.vocalink.crossproduct.shared.reference.ReferencesClient
 import com.vocalink.crossproduct.shared.settlement.SettlementsClient
 import org.junit.jupiter.api.Test
@@ -28,10 +22,7 @@ import org.junit.jupiter.api.Assertions.assertThrows
 
 class ClientFactoryTest {
 
-    private val cyclesClient: CyclesClient = mock(BPSCyclesClient::class.java)!!
     private val participantIOClient: ParticipantIODataClient = mock(BPSParticipantIODataClient::class.java)!!
-    private val participantClient: ParticipantClient = mock(BPSParticipantClient::class.java)!!
-    private val positionClient: PositionClient = mock(BPSPositionClient::class.java)!!
     private val alertsClient: AlertsClient = mock(BPSAlertsClient::class.java)!!
     private val filesClient: FilesClient = mock(BPSFilesClient::class.java)!!
     private val referencesClient: ReferencesClient = mock(BPSReferencesClient::class.java)!!
@@ -39,10 +30,7 @@ class ClientFactoryTest {
     private val settlementsClient: SettlementsClient = mock(BPSSettlementsClient::class.java)!!
 
     private val clientFactory: ClientFactory = ClientFactory(
-            listOf(participantClient),
-            listOf(cyclesClient),
             listOf(participantIOClient),
-            listOf(positionClient),
             listOf(alertsClient),
             listOf(filesClient),
             listOf(referencesClient),
@@ -51,39 +39,12 @@ class ClientFactoryTest {
     )
 
     @Test
-    fun `should get participant client`() {
-        `when`(participantClient.context).thenReturn(TestConstants.CONTEXT)
-        clientFactory.init()
-
-        val result = clientFactory.getParticipantClient(TestConstants.CONTEXT)
-        assertTrue(result is ParticipantClient)
-    }
-
-    @Test
-    fun `should get cycles client`() {
-        `when`(cyclesClient.context).thenReturn(TestConstants.CONTEXT)
-        clientFactory.init()
-
-        val result = clientFactory.getCyclesClient(TestConstants.CONTEXT)
-        assertTrue(result is CyclesClient)
-    }
-
-    @Test
     fun `should get participant IO client`() {
         `when`(participantIOClient.context).thenReturn(TestConstants.CONTEXT)
         clientFactory.init()
 
         val result = clientFactory.getParticipantIODataClient(TestConstants.CONTEXT)
         assertTrue(result is ParticipantIODataClient)
-    }
-
-    @Test
-    fun `should get positions client`() {
-        `when`(positionClient.context).thenReturn(TestConstants.CONTEXT)
-        clientFactory.init()
-
-        val result = clientFactory.getPositionClient(TestConstants.CONTEXT)
-        assertTrue(result is PositionClient)
     }
 
     @Test
@@ -105,34 +66,10 @@ class ClientFactoryTest {
     }
 
     @Test
-    fun `should throw participant client not available for wrong context`() {
-        clientFactory.init()
-        assertThrows(ClientNotAvailableException::class.java) {
-            clientFactory.getParticipantClient(TestConstants.CONTEXT)
-        }
-    }
-
-    @Test
-    fun `should throw cycles client not available for wrong context`() {
-        clientFactory.init()
-        assertThrows(ClientNotAvailableException::class.java) {
-            clientFactory.getCyclesClient(TestConstants.CONTEXT)
-        }
-    }
-
-    @Test
     fun `should throw participant IO client not available for wrong context`() {
         clientFactory.init()
         assertThrows(ClientNotAvailableException::class.java) {
             clientFactory.getParticipantIODataClient(TestConstants.CONTEXT)
-        }
-    }
-
-    @Test
-    fun `should throw position client not available for wrong context`() {
-        clientFactory.init()
-        assertThrows(ClientNotAvailableException::class.java) {
-            clientFactory.getPositionClient(TestConstants.CONTEXT)
         }
     }
 
