8d653755975f0eaa880a3649d9423c5337f8dcd5
Dmitrii Nedealcov <dmitrii.nedealcov@endava.com>
2021-01-22 11:45:18 +0200

    feat: added logging for http client

--- src/main/java/com/vocalink/crossproduct/infrastructure/bps/config/BPSClientConfig.java
+++ src/main/java/com/vocalink/crossproduct/infrastructure/bps/config/BPSClientConfig.java
@@ -3,16 +3,19 @@ package com.vocalink.crossproduct.infrastructure.bps.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.http.codec.json.Jackson2JsonDecoder;
 import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.channel.BootstrapHandlers;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class BPSClientConfig {
 
   @Bean
-  public WebClient webClient(ObjectMapper mapper) {
+  public WebClient webClient(ObjectMapper mapper, HttpClient httpClient) {
     ExchangeStrategies strategies = ExchangeStrategies.builder()
         .codecs(clientCodecConfigurer -> {
               clientCodecConfigurer.defaultCodecs()
@@ -23,6 +26,15 @@ public class BPSClientConfig {
         ).build();
     return WebClient.builder()
         .exchangeStrategies(strategies)
+        .clientConnector(new ReactorClientHttpConnector(httpClient))
         .build();
+  }
+
+  @Bean
+  public HttpClient httpClient() {
+    return HttpClient.create()
+        .tcpConfiguration(
+            tc -> tc.bootstrap(
+                b -> BootstrapHandlers.updateLogSupport(b, new BPSHttpLogger(HttpClient.class))));
   }
 }
