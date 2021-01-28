0003ebf2bd46aa71dd263c7dd577f938f10aef6d
Gleb Godonoga <gleb.godonoga@mastercard.com>
2021-01-28 09:55:05 +0200

    feat: adds cors config

---
+++ src/main/java/com/vocalink/crossproduct/infrastructure/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.vocalink.crossproduct.infrastructure.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ConditionalOnProperty(value = "app.cors", havingValue = "true")
+public class CorsConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer(@Value("${app.corsHosts}") String[] corsHosts) {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedHeaders("client-type", "context", "access-control-allow-headers", "content-type")
+            .allowedOrigins(corsHosts);
+      }
+    };
+  }
+}
