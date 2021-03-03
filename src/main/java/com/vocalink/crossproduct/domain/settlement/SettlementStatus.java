2d1c5458fa11a857ace8dcf89037fc16881ef459
Sandu Nicula <sandu.nicula@mastercard.com>
2021-03-03 17:02:41 +0200

    fix: added statuses to settlements

--- src/main/java/com/vocalink/crossproduct/domain/settlement/SettlementStatus.java
+++ src/main/java/com/vocalink/crossproduct/domain/settlement/SettlementStatus.java
@@ -1,6 +1,8 @@
 package com.vocalink.crossproduct.domain.settlement;
 
 public enum SettlementStatus {
+  CLOSED("closed"),
+  SETTLING("settling"),
   COMPLETED("completed"),
   PARTIAL("partial"),
   NO_RESPONSE("no-response");
