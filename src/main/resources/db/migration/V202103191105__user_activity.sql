14c0ff92f55fe71dbb2c63a9219a2f4ad4c53e85
Dmitrii Nedealcov <dmitrii.nedealcov@endava.com>
2021-03-19 16:20:38 +0200

    feat: added audit tables

---
+++ src/main/resources/db/migration/V202103191105__user_activity.sql
@@ -0,0 +1,12 @@
+create table user_activity
+(
+    id          varchar(36) not null,
+    name        varchar(30) not null,
+    description varchar(50) not null
+);
+
+alter table user_activity
+    add (
+        constraint ua_pk
+            primary key
+                (id));
