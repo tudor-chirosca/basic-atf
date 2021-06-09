f9510dcfb0263b176aa2b56eae87bbbba46e8900
Dmitrii Nedealcov <dmitrii.nedealcov@endava.com>
2021-06-09 11:09:55 +0300

    fix: replaced db query with method call

--- application/src/main/java/com/vocalink/crossproduct/infrastructure/actuator/AppInfoAdapter.java
+++ application/src/main/java/com/vocalink/crossproduct/infrastructure/actuator/AppInfoAdapter.java
@@ -4,6 +4,7 @@ import com.vocalink.crossproduct.infrastructure.jpa.info.DatabaseInfo;
 import com.vocalink.crossproduct.infrastructure.jpa.info.DatabaseVersionRepositoryJpa;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -20,11 +21,12 @@ public class AppInfoAdapter {
   private String pomVersion;
 
   public Map<String, String> getLatestMigrationVersion() {
-    final DatabaseInfo latestInfo = databaseVersionRepositoryJpa.getLatestInfo();
+    final Optional<DatabaseInfo> latestInfo = databaseVersionRepositoryJpa
+        .findFirstByOrderByReleaseDateDesc();
 
     final Map<String, String> map = new HashMap<>();
-    map.put("version", latestInfo == null ? NOT_AVAILABLE : latestInfo.getVersion());
-    map.put("releaseDate", latestInfo == null ? NOT_AVAILABLE : latestInfo.getReleaseDate());
+    map.put("version", latestInfo.map(DatabaseInfo::getVersion).orElse(NOT_AVAILABLE));
+    map.put("releaseDate", latestInfo.map(DatabaseInfo::getReleaseDate).orElse(NOT_AVAILABLE));
 
     return map;
   }
--- application/src/main/java/com/vocalink/crossproduct/infrastructure/jpa/info/DatabaseVersionRepositoryJpa.java
+++ application/src/main/java/com/vocalink/crossproduct/infrastructure/jpa/info/DatabaseVersionRepositoryJpa.java
@@ -1,10 +1,9 @@
 package com.vocalink.crossproduct.infrastructure.jpa.info;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface DatabaseVersionRepositoryJpa extends JpaRepository<DatabaseInfo, String> {
 
-  @Query("select di from DatabaseInfo di where di.releaseDate = (select max(di.releaseDate) from di)")
-  DatabaseInfo getLatestInfo();
+  Optional<DatabaseInfo> findFirstByOrderByReleaseDateDesc();
 }
