3986ffc45e5790765200688aabfc5847581e6b68
valentina-saban <86000612+valentina-saban@users.noreply.github.com>
2021-06-29 09:11:38 +0300

    feat: S2094409 - Create data provisioning for SAMA (#324)
    Co-authored-by: vsaban <56274858+vsaban@users.noreply.github.com>

--- database/migrations/data/sama/V202105050942__insert_role_ui_permissions.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO ROLE_UI_PERMISSION(role_id, ui_permission_id, participant_type)
-VALUES ((SELECT id FROM ROLE WHERE function = 'MANAGEMENT'), (SELECT id FROM UI_PERMISSION WHERE key = 'read.approvals-filters-participants-section'), 'SCHEME_OPERATOR');
