e122aa14568c025bcadc3a1738aca5f6cb9ac1e5
Hlistal Veaceslav <Veaceslav.Hlistal@mastercard.com>
2021-07-05 18:10:30 +0300

    fix: S2086394 - Added suspentionLevel field for /me

--- application/src/main/java/com/vocalink/crossproduct/ui/dto/participant/ParticipantDto.java
+++ application/src/main/java/com/vocalink/crossproduct/ui/dto/participant/ParticipantDto.java
@@ -1,8 +1,11 @@
 package com.vocalink.crossproduct.ui.dto.participant;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.vocalink.crossproduct.domain.participant.ParticipantStatus;
 import com.vocalink.crossproduct.domain.participant.ParticipantType;
+import com.vocalink.crossproduct.domain.participant.SuspensionLevel;
 import java.time.ZonedDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,6 +23,8 @@ public class ParticipantDto {
   private final ParticipantStatus status;
   private final ZonedDateTime suspendedTime;
   private final ParticipantType participantType;
+  @JsonInclude(Include.NON_EMPTY)
+  private final SuspensionLevel suspensionLevel;
 
   @JsonProperty("participantType")
   public String getParticipantType() {
