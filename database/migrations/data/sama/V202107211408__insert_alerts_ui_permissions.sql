d0acdacfab4da15657f0b39db7c4679f39f16b57
Alexandr-Vicev <87521894+Alexandr-Vicev@users.noreply.github.com>
2021-07-26 12:15:30 +0300

    feat: S2119982 - Roles and privileges for alerts (#389)

---
+++ database/migrations/data/sama/V202107211408__insert_alerts_ui_permissions.sql
@@ -0,0 +1,10 @@
+INSERT INTO UI_PERMISSION(id, key)
+VALUES ('244d5584-4898-4f53-8294-e5ad9e39c799', 'read.delay-eod-report');
+INSERT INTO UI_PERMISSION(id, key)
+VALUES ('9c4eea90-28ae-46f9-a363-f2cd0c741ce5', 'read.timeout-settlement-processing');
+INSERT INTO UI_PERMISSION(id, key)
+VALUES ('ebda66d9-8ddd-454f-862e-eb7d03f84f90', 'read.central-bank-reject-message');
+INSERT INTO UI_PERMISSION(id, key)
+VALUES ('8862d780-b136-44fd-bf51-dbdcb0de7c81', 'read.central-bank-reject-message-received');
+INSERT INTO UI_PERMISSION(id, key)
+VALUES ('2c689f74-9178-4240-9484-0070be1fa28a', 'read.output-files-not-generated-with-schedule');
